dc904872fd45add59d4f106941aaed4763bceea3
HUYNH Viet Phi <vietphi3892@gmail.com>
2019-10-11 23:17:58 +0200

    Delete run_generator.sh

--- public/Benchmark/Extract_metadata/run_generator.sh
+++
@@ -1,9 +0,0 @@
-python scenario_generator_v2.py ../data/dbpedia/ -r foundedBy -t train -sc 600 -hm 1.0 -cs 0.0 -pn 1.0 -p random -f ../data/dbpedia/splits/foundedBy/train_c0.0_h1.0_s600_r1.0_random_3_scenario.tsv -c 3
-python scenario_generator_v2.py ../data/dbpedia/ -r foundedBy -t train -sc 600 -hm 1.0 -cs 0.0 -pn 1.0 -p random -f ../data/dbpedia/splits/foundedBy/train_c0.0_h1.0_s600_r1.0_random_4_scenario.tsv -c 4
-python scenario_generator_v2.py ../data/dbpedia/ -r foundedBy -t train -sc 600 -hm 1.0 -cs 0.0 -pn 1.0 -p random -f ../data/dbpedia/splits/foundedBy/train_c0.0_h1.0_s600_r1.0_random_0_scenario.tsv -c 0
-python scenario_generator_v2.py ../data/dbpedia/ -r spouse -t train -sc 600 -hm 1.0 -cs 0.0 -pn 1.0 -p random -f ../data/dbpedia/splits/spouse/train_c0.0_h1.0_s600_r1.0_random_5_scenario.tsv -c 5
-python scenario_generator_v2.py ../data/dbpedia/ -r spouse -t train -sc 600 -hm 1.0 -cs 0.0 -pn 1.0 -p random -f ../data/dbpedia/splits/spouse/train_c0.0_h1.0_s600_r1.0_random_7_scenario.tsv -c 7
-python scenario_generator_v2.py ../data/dbpedia/ -r spouse -t train -sc 600 -hm 1.0 -cs 0.0 -pn 1.0 -p random -f ../data/dbpedia/splits/spouse/train_c0.0_h1.0_s600_r1.0_random_15_scenario.tsv -c 15
-
-
-
